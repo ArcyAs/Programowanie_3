bd70d077b832ea1ebfef86d958f3c37969f461b3
Arcy As <serilmagnifico@gmail.com>
2019-09-28 19:36:40 +0200

    Start

---
+++ Programowanie_3/Programowanie_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Programowanie_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+//Zmiana
