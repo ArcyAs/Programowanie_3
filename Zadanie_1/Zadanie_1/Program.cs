9e6bceef496133d8545feda4f8d1fd303bea80fc
Arcy As <serilmagnifico@gmail.com>
2019-10-18 16:17:16 +0200

    V3.1

---
+++ Zadanie_1/Zadanie_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class Funkcja
+{
+     public static void Wypisz()
+    {
+        Console.WriteLine("Dowolny");
+    }
+    
+
+}
+
+namespace Zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funkcja.Wypisz();
+        }
+    }
+}
